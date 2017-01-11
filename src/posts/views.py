41e533f00f0e9bc08b50c8f7783568ab12b96675
cropse <stu0219@hotmail.com>
2017-01-11 18:13:32 +0800

    sample get the object to queryset with get_object_or_404

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from .models import Post
@@ -8,10 +8,13 @@ def post_create(request):
     return HttpResponse("<h1>create</h1>")
 
 def post_detail(request):# retrieve
+    # instance = Post.objects.get(id=2)
+    instance = get_object_or_404(Post, id="3")
     context = {
-        'title': "Detail",
+        'instance': instance,
+        'title': instance.title,
     }
-    return render(request, "index.html", context)
+    return render(request, "post_detail.html", context)
 
 def post_list(request):# list item
     # if request.user.is_authenticated():
