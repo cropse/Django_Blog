315a2221f0621c8957de5184de3780d45731365d
cropse <stu0219@hotmail.com>
2017-01-11 17:08:57 +0800

    adding urls path and fix BAS_DIR simple template added

---
+++ src/posts/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import url
+from django.contrib import admin
+
+from .views import (
+    post_create,
+    post_detail,
+    post_list,
+    post_update,
+    post_delete,
+    )
+
+urlpatterns = [
+    # url(r'^$', views.post_detail ),
+    url(r'^create/$', post_create),
+    url(r'^detail/$', post_detail),
+    url(r'^list/$', post_list),
+    url(r'^update/$', post_update),
+    url(r'^delete/$', post_delete),
+]
--- src/posts/views.py
+++ src/posts/views.py
@@ -1,3 +1,19 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def post_create(request):
+    return HttpResponse("<h1>create</h1>")
+
+def post_detail(request):# retrieve
+    return render(request, "index.html")
+
+def post_list(request):# list item
+    return HttpResponse("<h1>list</h1>")
+
+def post_update(request):
+    return HttpResponse("<h1>Hello</h1>")
+
+def post_delete(request):
+    return HttpResponse("<h1>Hello</h1>")
