7cf3b7d942316b76589a48f9e5959cc18a892567
cropse <stu0219@hotmail.com>
2017-01-16 17:08:23 +0800

    C2-32 Pagination page_list and set meta ordering in model

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,6 +1,7 @@
 from django.contrib import messages
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 from .models import Post
@@ -29,12 +30,28 @@ def post_detail(request, id=None):# retrieve
     return render(request, "post_detail.html", context)
 
 def post_list(request):# list item
-    queryset = Post.objects.all()
+    queryset_list = Post.objects.all()
+    paginator = Paginator(queryset_list, 6) # Show 25 contacts per page
+    page_request_var = 'page'
+    page = request.GET.get(page_request_var)
+    try:
+        queryset = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        queryset = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        queryset = paginator.page(paginator.num_pages)
+
     context = {
         'object_list': queryset,
         'title': "List",
+        'page_request_var': page_request_var,
     }
     return render(request, "post_list.html", context)
+
+
+
 
 def post_update(request, id=None):
     instance = get_object_or_404(Post, id=id)
