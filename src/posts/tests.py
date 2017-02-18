3299e62a26865b60afdc3c9d8cc6c8a1d417789c
cropse <stu0219@hotmail.com>
2017-02-18 13:34:26 +0800

    add Test of markdown&linebreaks change block name for static file and title

--- src/posts/tests.py
+++ src/posts/tests.py
@@ -4,7 +4,8 @@ from zinnia.models import Entry
 
 class EntryTestCase(TestCase):
     def setUp(self):
-        Entry.objects.create(title="test", content="#Title Test \n 123")
+        Entry.objects.create(title="test_p_id", content="""
+title\n{: #title}""")
         Entry.objects.create(title="test_checkbox", content="""
 and the source code is also on [Github][BlogGithub]
 **notice: the site is on beta**
@@ -15,12 +16,11 @@ and the source code is also on [Github][BlogGithub]
 - [x] Earth (Orbit/Moon)")
 [BlogGithub]: www.cropse.com
 """)
-        # print(Entry.objects.get(title="test_checkbox").get_markdown())
+        Entry.objects.create(title="test_class", content="""**123**\n456\n**789**\n012\n
+`testclass`{: .added}""")
 
-    def test_entry_markdown(self):
-        Entrytest = Entry.objects.get(title="test")
+    def test_entry_checkbox(self):
         Checkboxtest = Entry.objects.get(title="test_checkbox")
-        self.assertEqual(Entrytest.get_markdown(), '<h1>Title Test</h1>\n<p>123</p>')
         self.assertEqual(Checkboxtest.get_markdown(), 
 """<p>and the source code is also on <a href="www.cropse.com">Github</a>
 <strong>notice: the site is on beta</strong></p>
@@ -33,15 +33,20 @@ and the source code is also on [Github][BlogGithub]
 </ul>""")
 
     def test_slug(self):
-        slug_test = Entry.objects.get(title="test")
-        self.assertEqual(slug_test.slug, 'test')
+        slug_test = Entry.objects.get(title="test_p_id")
+        self.assertEqual(slug_test.slug, 'test_p_id')
+
+    def test_class(self):
+        response = self.client.get('/posts/test_class/')
+        self.assertContains(response, """<code class="added">testclass</code>""")
+        self.assertContains(response, "<p><strong>123</strong><br />456<br /><strong>789</strong><br />012</p>")
 
     def test_response(self):
-        response = self.client.get('/posts/test/')
+        response = self.client.get('/posts/test_p_id/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'base/base.html')
         self.assertTemplateUsed(response, 'posts/post_detail.html')
-        self.assertContains(response, '<h1>Title Test</h1>\n<p>123</p>')
+        self.assertContains(response, '<p id="title">title</p>')
         response_list = self.client.get('/posts/')
         self.assertEqual(response_list.status_code, 200)
         self.assertTemplateUsed(response_list, 'base/base.html')
