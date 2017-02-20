c24d2a1405ea1bb4092a18e979f3bb8576682169
cropse <stu0219@hotmail.com>
2017-02-20 13:42:48 +0800

    Fix markdown br new selenium test

--- src/pages/tests.py
+++ src/pages/tests.py
@@ -1,3 +1,69 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.contrib.auth.models import User
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest, time, re
+
+class MySeleniumTests(StaticLiveServerTestCase):
+    # fixtures = ['user-data.json']
+
+    @classmethod
+    def setUpClass(cls):
+        User.objects.create_superuser("admin", "admin@admin.com", "admin")
+        
+        cls.driver = webdriver.Firefox()
+        cls.driver.implicitly_wait(30)
+        # cls.base_url = cls.live_server_url
+        cls.verificationErrors = []
+        cls.accept_next_alert = True
+
+        super(MySeleniumTests, cls).setUpClass()
+
+        # cls.driver = WebDriver()
+        cls.driver.implicitly_wait(5)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+        super(MySeleniumTests, cls).tearDownClass()
+
+    def test_login(self):
+        self.driver.get('%s%s' % (self.live_server_url, '/admin/'))
+        username_input = self.driver.find_element_by_name("username")
+        username_input.send_keys('admin')
+        password_input = self.driver.find_element_by_name("password")
+        password_input.send_keys('admin')
+        self.driver.find_element_by_xpath('//input[@value="Log in"]').click()
+        for i in range(30):
+            try:
+                if "Site administration" == self.driver.find_element_by_css_selector("#content > h1").text: break
+            except: pass
+            time.sleep(1)
+        else: self.fail("time out")
+
+        self.driver.implicitly_wait(5)
+
+        # post test
+        self.driver.get(self.live_server_url + "/posts/create/")
+        self.driver.find_element_by_id("id_title").clear()
+        self.driver.find_element_by_id("id_title").send_keys("Test")
+        self.driver.find_element_by_id("id_content").clear()
+        self.driver.find_element_by_id("id_content").send_keys("# yayaya")
+        self.driver.find_element_by_id("id_tags").clear()
+        self.driver.find_element_by_id("id_tags").send_keys("okok, 123")
+        Select(self.driver.find_element_by_id("id_status")).select_by_visible_text("published")
+        self.driver.find_element_by_css_selector("input.btn.btn-default").click()
+        for i in range(30):
+            try:
+                if "yayaya" == self.driver.find_element_by_css_selector("div.post-detail-item > h1").text: break
+            except: pass
+            time.sleep(1)
+        else: self.fail("time out")
--- src/posts/tests.py
+++ src/posts/tests.py
@@ -20,17 +20,9 @@ and the source code is also on [Github][BlogGithub]
 `testclass`{: .added}""")
 
     def test_entry_checkbox(self):
-        Checkboxtest = Entry.objects.get(title="test_checkbox")
-        self.assertEqual(Checkboxtest.get_markdown(), 
-"""<p>and the source code is also on <a href="www.cropse.com">Github</a>
-<strong>notice: the site is on beta</strong></p>
-<h3>Todo list</h3>
-<hr />
-<ul class="checklist">
-<li><input type="checkbox" disabled> Mercury</li>
-<li><input type="checkbox" disabled checked> Venus</li>
-<li><input type="checkbox" disabled checked> Earth (Orbit/Moon)")</li>
-</ul>""")
+        response = self.client.get('/posts/test_checkbox/')
+        # print(response.content)
+        self.assertContains(response, """<li><input type="checkbox" disabled> Mercury</li>\n<li><input type="checkbox" disabled checked> Venus</li>\n<li><input type="checkbox" disabled checked> Earth (Orbit/Moon)")</li>""")
 
     def test_slug(self):
         slug_test = Entry.objects.get(title="test_p_id")
@@ -39,14 +31,15 @@ and the source code is also on [Github][BlogGithub]
     def test_class(self):
         response = self.client.get('/posts/test_class/')
         self.assertContains(response, """<code class="added">testclass</code>""")
-        self.assertContains(response, "<p><strong>123</strong><br />456<br /><strong>789</strong><br />012</p>")
+        # print(response.content)
+        self.assertContains(response, "<p><strong>123</strong><br />\n456<br />\n<strong>789</strong><br />\n012</p>")
 
     def test_response(self):
         response = self.client.get('/posts/test_p_id/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'base/base.html')
         self.assertTemplateUsed(response, 'posts/post_detail.html')
-        self.assertContains(response, '<p id="title">title</p>')
+        self.assertContains(response, '<p id="title">title<br /></p>')
         response_list = self.client.get('/posts/')
         self.assertEqual(response_list.status_code, 200)
         self.assertTemplateUsed(response_list, 'base/base.html')
